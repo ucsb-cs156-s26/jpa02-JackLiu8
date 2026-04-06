e8669abc2f2115ca00011ede6c01b5a5938f65f4
Jack Liu <jliu96@ucsb.edu>
2026-04-06 15:11:36 -0700

    xy - updated with actual github id

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -29,5 +29,9 @@ public class DeveloperTest {
 
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void getGithubId_returns_correct_githubId() {
+        assertEquals("JackLiu8", Developer.getGithubId());
+    }
 
 }
